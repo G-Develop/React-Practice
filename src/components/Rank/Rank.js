468c126d268ea4485f6fb4b6f170a0dcd05d8e17
G-Develop <mfaisal.rahman51.996@gmail.com>
2019-05-15 19:55:46 +0000

    added particles-js lib

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`your current  count is...`}
+      </div>
+      <div className='white f1'>
+        {'#5'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank
